21c7ed3f34736f040db996c614ed9497b6042d5a
Jian Liu <jianliu@JiandeMacBook-Pro.local>
2022-12-06 22:05:55 +0800

    feat(main):  - initial process entrypoint  - initial cobra framework

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+var rootCmd = &cobra.Command{
+	Use:   "root",
+	Short: "",
+	Long:  ``,
+}
+
+func init() {
+
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
