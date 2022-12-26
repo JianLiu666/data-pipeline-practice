3cb759d560be30f22c6a50f47cb14eff97b69b13
Jian Liu <jianliu0616@gmail.com>
2022-12-26 20:28:53 +0800

    fix(concurrent transfer):  - 調整併發情境流程

--- cmd/concurrent_transfer_case.go
+++ cmd/concurrent_transfer_case.go
@@ -26,52 +26,67 @@ func init() {
 func RunConcurrentTransferCmd(cmd *cobra.Command, args []string) error {
 	ctx := context.Background()
 
-	infra := accessor.BuildAccessor()
-	defer infra.Close(ctx)
+	infra1 := accessor.BuildAccessor()
+	defer infra1.Close(ctx)
+	infra1.InitRDB(ctx)
 
-	infra.InitRDB(ctx)
+	infra2 := accessor.BuildAccessor()
+	defer infra2.Close(ctx)
+	infra2.InitRDB(ctx)
 
 	// business logic
 
-	// 模擬併發交易情境
-	// local 維護一組資料, 併發寫入 DB 後跟 DB 比較兩邊交易結果是否一致
-
-	statements := `
-	-- BEGIN;
-	
+	// UPDATE ... WHERE ... 命令屬於排他鎖
+	// desc: sets an exclusive next-key lock on every record the search encounters.
+	statements := `	
 	-- transaction
 	UPDATE wallets INNER JOIN users ON wallets.user_id = users.id SET amount = amount + 1 WHERE users.id = 1;
 	UPDATE wallets INNER JOIN users ON wallets.user_id = users.id SET amount = amount - 1 WHERE users.id = %v;
 	
 	-- logging
 	INSERT INTO logs (deposit_user_id, withdraw_user_id, amount, created_at) VALUES (1, 2, 1, '%v');
-	
-	-- COMMIT;
 	`
 
+	// 模擬併發交易情境
+
 	wg := new(sync.WaitGroup)
-	limit := 10000
-	wg.Add(limit - 1)
+	wg.Add(2)
 
-	for i := 2; i <= limit; i++ {
-		go func(_wg *sync.WaitGroup, idx int) {
-			defer _wg.Done()
+	go func(_wg *sync.WaitGroup) {
+		defer _wg.Done()
 
-			tx, err := infra.RDB.Conn.BeginTx(context.TODO(), &sql.TxOptions{Isolation: sql.LevelReadUncommitted})
-			if err != nil {
-				logrus.Panicf("failed to start transaction: %v", err)
-			}
+		tx1, err := infra1.RDB.Conn.Begin()
+		if err != nil {
+			logrus.Panicf("failed to start transaction: %v", err)
+		}
 
-			if _, err := tx.Exec(fmt.Sprintf(statements, idx, time.Now().Format("2006-01-02 15:04:05"))); err != nil {
-				logrus.Panicf("failed to execute sql task: %v", err)
-			}
+		if _, err := tx1.Exec(fmt.Sprintf(statements, 2, time.Now().Format("2006-01-02 15:04:05"))); err != nil {
+			logrus.Panicf("failed to execute sql task: %v", err)
+		}
 
-			if err := tx.Commit(); err != nil {
-				logrus.Panicf("failed to commit transaction: %v", err)
-			}
+		time.Sleep(3 * time.Second)
 
-		}(wg, i)
-	}
+		if err := tx1.Rollback(); err != nil {
+			logrus.Panicf("failed to rollback transaction: %v", err)
+		}
+	}(wg)
+
+	go func(_wg *sync.WaitGroup) {
+		defer _wg.Done()
+
+		tx2, err := infra2.RDB.Conn.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelReadUncommitted})
+		if err != nil {
+			logrus.Panicf("failed to start transaction: %v", err)
+		}
+
+		if _, err := tx2.Exec(fmt.Sprintf(statements, 2, time.Now().Format("2006-01-02 15:04:05"))); err != nil {
+			logrus.Panicf("failed to execute sql task: %v", err)
+		}
+
+		if err := tx2.Commit(); err != nil {
+			logrus.Panicf("failed to commit transaction: %v", err)
+		}
+	}(wg)
 
 	wg.Wait()
 
